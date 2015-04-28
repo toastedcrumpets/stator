ce183989228f8232f68b6542c89fcf947769cc76
Marcus Bannerman <m.bannerman@gmail.com>
2015-04-28 08:04:06 +0100

    Adding the test configuration file

---
+++ CTestConfig.cmake
@@ -0,0 +1,10 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+set(CTEST_PROJECT_NAME "stator")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "cdash.dynamomd.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=stator")
+set(CTEST_DROP_SITE_CDASH TRUE)
